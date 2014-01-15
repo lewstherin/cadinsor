44dcbbfc9f91a372960ec61dad5f208d4d09e9e8
lewstherin <r@lewstherin.com>
2014-01-16 00:49:30 +0530

    mend

--- lib/generators/cadinsor/install/install_generator.rb
+++ lib/generators/cadinsor/install/install_generator.rb
@@ -1,6 +1,5 @@
 class Cadinsor::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
-  desc "RailsAdmin installation generator"
   def install
     puts "Creating an initializer for Cadinsor..."
     template "cadinsor_initializer.erb", "config/initializers/cadinsor.rb"
